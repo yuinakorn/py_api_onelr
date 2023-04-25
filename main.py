ecbcf6e0617082a84aea15e9c658fa9b42ef2a27
yuinakorn <yuinakorn@hotmail.com>
2023-04-25 11:27:44 +0700

    update c

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ from routers import pregs_router
 # from routers.items import items_router
 from models.database import engine
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+# Allow requests from all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(pregs_router.router)
-
